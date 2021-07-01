fd704c895b2f7765002bba966b83e73cb2b7bd6d
AnnFyz <annfyz1232@yandex.ru>
2021-07-02 01:11:14 +0300

    addedText

--- public/mymachines/wave/sketch.js
+++ public/mymachines/wave/sketch.js
@@ -6,11 +6,11 @@ var flatlandConfig = {
   server: "https://flatland.earth",
   land: "default",
   updateIntervall: 30,
-  spawnIntervall: 100,
+  spawnIntervall: 40,
   debug: false,
-  clearscreen: false,
-  backgroundcolor: [255, 255, 255],
-  backgroundblend: 0.5,
+  clearscreen: true,
+  backgroundcolor: [208, 208, 208],
+  backgroundblend: 0.1,
 };
 
 var machineConfig = {
@@ -19,75 +19,57 @@ var machineConfig = {
   minSize: 20,
   maxSize: 30,
   lifetime: 10000, // forever...!
-  color1: [255, 0, 255],
-  color1Opacity: 0.1,
-  color2: [0, 255, 255],
-  color2Opacity: 0.1,
-  pendown: false,
+  color1: [0, 200, 255],
+  color1Opacity: 0.7,
+  color2: [0, 200, 255],
+  color2Opacity: 0.7,
+  pendown: true,
 };
 
 let angles = [];
 let angleV = [];
-let r = 10;
+let r = 20;
 // ---------------------------------------------------------------
 class Machine extends defaultMachine {
   setup() {
-    // initialize your machine
+    this.setPenDown();
     this.setType(MachineType.CIRCLE);
-    let total = floor(width / (r * 2));
-    for (let i = 0; i < total + 1; i++) {
-      angles[i] = map(i, 0, total, 0, 2 * TWO_PI);
-      // angleV[i] = 0.01 + i / 100;
-    }
+    this.setLifetime(10000000); //forever
   }
 
   move() {
-    beginShape();
     for (let i = 0; i < angles.length; i++) {
       let y = map(sin(angles[i]), -1, 1, -200, 200);
       strokeWeight(4);
       let x = map(i, 0, angles.length, -300, 300);
-      // line(x, 0, x, y);
+      //line(x, 0, x, y);
+      this.setPosition(x, y);
+      this.setSize(r * 2);
       circle(x, y, r * 2);
-      // vertex(x,y);
-      angles[i] += 0.02;
+
+      angles[i] += 0.002;
       // angles[i] += angleV[i];
     }
-    endShape();
   }
 }
 // --------------------------------------------------------------
 
 let gui;
 let flatland;
-
-// my own  gloabal variables
-let maxpoints = 7;
-let index = 0;
-let liquid = [];
-
-let centerOfSystemX = 0;
-let centerOfSystemY = 0;
 // local stuff
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  // array of bubbles
-  for (var bb = 0; bb < maxpoints; bb++) {
-    var v = createVector(centerOfSystemX, centerOfSystemY);
-    liquid.push(v);
-  }
-
-  machineConfig.maxCount = liquid.length;
-
   flatland = new Flatland(); // connect to the flatland server
   initGui();
   initSocketIO(flatlandConfig.server);
+
+  let total = floor(width / (r * 3));
+  for (let i = 0; i < total + 1; i++) {
+    angles[i] = map(i, 0, total, 0, 2 * TWO_PI);
+    // angleV[i] = 0.01 + i / 100;
+  }
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    centerOfSystemX = mouseX - width / 2;
-    centerOfSystemY = mouseY - height / 2;
-  }
   flatland.update(); // update + draw flatland
 }
